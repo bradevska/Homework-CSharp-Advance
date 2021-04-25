a083d4982d80dd52ba6857f0e9d06178536879c3
biljana.radevska <b.radevska@yahoo.com>
2021-04-25 13:24:40 +0200

    Add project files.

---
+++ CSharpAdvance.Class2.Homework/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpAdvance.Class2.Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
