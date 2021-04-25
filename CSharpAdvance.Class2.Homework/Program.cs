c773be7072e74a115c25c79dcd449d34c9913ef9
biljana.radevska <b.radevska@yahoo.com>
2021-04-25 13:40:37 +0200

    Change 25.04.2021

--- CSharpAdvance.Class2.Homework/Program.cs
+++ CSharpAdvance.Class2.Homework/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using CSharpAdvance.Class2.Homework.Classes;
+using CSharpAdvance.Class2.Homework.Interfaces;
+using System;
+using System.Collections.Generic;
 
 namespace CSharpAdvance.Class2.Homework
 {
@@ -6,7 +9,22 @@ namespace CSharpAdvance.Class2.Homework
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<int> bobGrades = new List<int> { 7, 9, 6, 8 };
+            List<int> jilGrades = new List<int> { 6, 7, 9, 9 };
+            IStudent bob = new Student(4, "Bob", "bob", "bob123", bobGrades);
+            IStudent jil = new Student(7, "Jil", "jil", "jil321", jilGrades);
+            ITeacher nikola = new Teacher(2, "Nikola", "nik", "nik123", "C#Basic");
+            ITeacher trajan = new Teacher(1, "Trajan", "tra", "tra123", "C#Advance");
+
+            Console.WriteLine(bob.PrintUser());
+            Console.WriteLine(jil.PrintUser());
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine(nikola.PrintUser());
+            Console.WriteLine(trajan.PrintUser());
+
+
+
+            Console.ReadLine();
         }
     }
 }
